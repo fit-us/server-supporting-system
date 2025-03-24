5511958300eadae443ced6eb485ced50c9d5571a
KNU-K <knukang334@gmail.com>
2025-03-25 05:59:41 +0900

    feat : JSON 직렬화 및 역직렬화 기능 추가

---
+++ cbt-bot/json_serializer.py
@@ -0,0 +1,17 @@
+import json
+
+class JsonSerializer:
+    @staticmethod
+    def to_json(obj):
+        return json.dumps(obj)
+    
+    @staticmethod
+    def from_json(json_str):
+        try:
+            if json_str.startswith("```json"):
+                json_str = json_str[7:]
+            if json_str.endswith("```"):
+                json_str = json_str[:-3]
+            return  json.loads(json_str.strip())
+        except json.JSONDecodeError as e:
+            print(f"JSON 디코딩 오류: {e}")
