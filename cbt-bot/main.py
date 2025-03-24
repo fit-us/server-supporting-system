79661dd5ec451001723d19af75b370b2af24b62d
KNU-K <knukang334@gmail.com>
2025-03-25 06:02:20 +0900

    feat : CBTBot 클래스 추가 및 대화 기능 구현

---
+++ cbt-bot/main.py
@@ -0,0 +1,41 @@
+from langchain.memory import ConversationBufferMemory
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dataclasses import dataclass
+from markdown_utils import MarkdownReader
+from config import Config
+from langchain.schema import HumanMessage, AIMessage
+
+@dataclass
+class CBTBot:
+    llm: ChatGoogleGenerativeAI
+    prompt: str
+    
+    def __init__(self):
+        self.llm = ChatGoogleGenerativeAI(
+            model=Config.MODEL,
+            temperature=Config.TEMPERATURE,
+            max_output_tokens=Config.MAX_OUTPUT_TOKENS,
+            google_api_key=Config.GOOGLE_API_KEY
+        )
+        self.prompt = MarkdownReader.read_file(Config.PROMPT_PATH)
+    
+    def invoke(self, message: str):
+        # 기억된 대화를 포함하여 모델에 전달
+        messages = [{"role": "system", "content": self.prompt}]
+        messages.append({"role": "user", "content": message})
+        response = self.llm.invoke(messages)
+
+        return response
+
+if __name__ == "__main__":
+    bot = CBTBot()
+
+    while True:
+        # 사용자로부터 메시지 입력 받기
+        user_message = input("사용자: ")
+        if user_message.lower() == "exit":  # 사용자가 'exit' 입력 시 종료
+            break
+
+        # 봇의 응답 출력
+        response = bot.invoke(user_message)
+        print(f"봇: {response.content}")
