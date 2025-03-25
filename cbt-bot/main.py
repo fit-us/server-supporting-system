7fb3a9e21b05e3f57d6a25309367a276a14f870f
KNU-K <knukang334@gmail.com>
2025-03-26 02:19:09 +0900

    refactor : main.py에서 주석 처리된 코드 제거

--- cbt-bot/main.py
+++ cbt-bot/main.py
@@ -46,14 +46,3 @@ def serve():
 
 if __name__ == "__main__":
     serve()
-    # bot = CBTBotLLM()
-
-    # while True:
-    #     # 사용자로부터 메시지 입력 받기
-    #     user_message = input("사용자: ")
-    #     if user_message.lower() == "exit":  # 사용자가 'exit' 입력 시 종료
-    #         break
-
-    #     # 봇의 응답 출력
-    #     response = bot.invoke(user_message)
-    #     print(f"봇: {response}")
