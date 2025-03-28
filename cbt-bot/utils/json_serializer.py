38aae736fa448248e8a6624d0e43a959317fee9a
KNU-K <knukang334@gmail.com>
2025-03-28 16:57:52 +0900

    fix: JSON 디코딩 오류 로깅 개선 및 JSON 문자열 추출 정규 표현식 추가

--- cbt-bot/utils/json_serializer.py
+++ cbt-bot/utils/json_serializer.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import re
 
 class JsonSerializer:
     @staticmethod
@@ -11,13 +12,14 @@ class JsonSerializer:
         if not isinstance(data_type, type):
             raise TypeError("data_type must be a class.")     
         try:
+            json_str = re.search(r'```json\n(.*?)\n```', json_str, re.DOTALL).group(1)
             if json_str.startswith("```json"):
                 json_str = json_str[7:]
             if json_str.endswith("```"):
                 json_str = json_str[:-3]
             return  data_type(**json.loads(json_str.strip()))
         except json.JSONDecodeError as e:
-            logging.error(f"JSON 디코딩 오류: {e}")
+            logging.error(f"JSON 디코딩 오류: {e} - {data_type}{json_str}")
             return None
         except Exception as e:
             logging.error(f"예상치 못한 오류: {e}")
